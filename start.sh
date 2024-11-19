dcdda6828f3303e3d9bb5977617c7ab5fcdf8d7b
Minh Nhat <Minhnhat1692@gmail.com>
2024-11-20 01:18:33 +0700

    update docker for live reload

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 cd /var/www/pandacode && composer install
-
 #exec /usr/bin/supervisord -c /etc/supervisord.conf > /dev/null
 exec /usr/bin/supervisord -c /etc/supervisord.conf &
 
 # Wait a bit to ensure supervisord has started before continuing
-
 cd /var/www/pandacode/web/app/themes/pandacode && composer install && yarn && yarn dev
